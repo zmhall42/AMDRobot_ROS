7a1bb8087a46e520b94401148a2f18ba008f55c4
Zach Hall <zmhall42@tntech.edu>
2021-10-27 16:53:39 -0400

    Update Tank_Drive_01.ino
    Works with teleop_twist_keyboard node.

--- Arduino Code/Tank_Drive_01/Tank_Drive_01.ino
+++ Arduino Code/Tank_Drive_01/Tank_Drive_01.ino
@@ -19,18 +19,30 @@
  *     line 182 from std::memcpy to memcpy
  */
 #include <geometry_msgs/Twist.h>  //Use for drive message read
+#include <Kinematics.h> //converts linear x and angular z to left and right rotation speeds
 
 //Arduino pin Setup
 #define L_PWM 11
 #define L_DIR 12
 #define R_PWM 9
 #define R_DIR 8
+#define LED_PIN 13
 
-//Variables
-int linear_vel = 0; //init at 0, positive is forward
-int angular_vel = 0; //init at 0, positive is CCW
-const float dist_between_wheels = 0.1; //in m, center to center
-const int max_speed_each_side = 1; //in m/s
+//Kinematics Variables
+#define LR_WHEEL_DISTANCE 0.242  //meters, center to center
+#define MAX_MOTOR_RPM 560 //560 under max load, 617 no load
+#define PWM_BITS 8  //set for Arduino Uno and Mega
+#define WHEEL_DIAMETER 0.048 //meters from trough between teeth across center to trough between teeth
+#define FR_WHEEL_DISTANCE 0 //considered to be a 2 wheeled robot
+
+//System Variables
+#define MAX_PWM_WRITE 255 //max value to use with PWM analogWrite function
+#define LEFT_FWD false  //reversed due to construction
+#define RIGHT_FWD true
+
+//Placeholders
+float linear_vel = 0.000; //init at 0, positive is forward m/s
+float angular_vel = 0.000; //init at 0, positive is CCW m/s
 bool twistCb_flag = false;
 
 //ROS Defines ----------------------------------------------------------------------
@@ -39,49 +51,40 @@ void twistCallback(const geometry_msgs::Twist& drive_vel) {
   angular_vel = drive_vel.angular.z;
 
   twistCb_flag = true;
+  
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN)); //test - toggle when ran
 }
 
 ros::NodeHandle nh; //create node handle for local use
 ros::Subscriber<geometry_msgs::Twist> sub_velocity("cmd_vel", twistCallback); //create subscriber to cmd_vel
 
+// Kinematic Defines ---------------------------------------------------------------
+Kinematics motors(MAX_MOTOR_RPM, WHEEL_DIAMETER, FR_WHEEL_DISTANCE, LR_WHEEL_DISTANCE, PWM_BITS);
+
 //Functions ------------------------------------------------------------------------
-void motor_write(int linear, int angular)  {
-  //convert linear and angular to per motor speed --> motor = linear + (angular*dist between tracks)/2
-  int left_vel = linear + (angular*dist_between_wheels)/2;  //If turn is backwards, change the +/- for this and next line
-  int right_vel = linear - (angular*dist_between_wheels)/2;
+void motor_write(int left_PWM, int right_PWM)  {
+  //Get motor's directions
+  bool left_dir;
+  if(left_PWM >= 0) left_dir = LEFT_FWD;
+  else left_dir = !LEFT_FWD;
 
-  //set to max if too large (pos or neg)
-  if(left_vel > max_speed_each_side) left_vel = max_speed_each_side;
-  else if(left_vel < -max_speed_each_side) left_vel = -max_speed_each_side;
-  
-  if(right_vel > max_speed_each_side) right_vel = max_speed_each_side;
-  else if(right_vel < -max_speed_each_side) right_vel = -max_speed_each_side;
+  bool right_dir;
+  if(right_PWM >= 0) right_dir = RIGHT_FWD;
+  else right_dir = !RIGHT_FWD;
 
-  //adjust - scale each speed to the PWM range of the motor drivers
-  int left_vel_adj = map(left_vel, -max_speed_each_side, max_speed_each_side, -255, 255); //255 is max PWM duty cycle
-  int right_vel_adj = map(right_vel, -max_speed_each_side, max_speed_each_side, -255, 255);
-
-  //left
-  bool left_fwd;  //define direction variable
-  int left_speed = abs(left_vel_adj); //set speed to the abs value of the adjusted velocity
-  if(left_vel_adj > 0)  left_fwd = false; //if positive velocity, go forward
-  else  left_fwd = true;  //otherwise, negative velocity and go backwards
-  
-  //right, same as left side above with opposite sign for direction
-  bool right_fwd;
-  int right_speed = abs(right_vel_adj);
-  if(right_vel_adj > 0) right_fwd = true;
-  else  right_fwd = false;
-
-  //write the speed to the motors
-  digitalWrite(L_DIR, left_fwd);
-  analogWrite(L_PWM, left_speed);
-  digitalWrite(R_DIR, right_fwd);
-  analogWrite(R_PWM, right_speed);
+  //Write the speed to the motors
+  digitalWrite(L_DIR, left_dir);
+  analogWrite(L_PWM, abs(left_PWM));
+  digitalWrite(R_DIR, right_dir);
+  analogWrite(R_PWM, abs(right_PWM));
 }
 
 //Setup ----------------------------------------------------------------------------
 void setup() {
+  //Set up test led
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
   //DIR Pin setup
   pinMode(L_DIR, OUTPUT); //set DIR pins to OUTPUTS
   pinMode(R_DIR, OUTPUT);
@@ -102,10 +105,16 @@ void setup() {
 //Main Loop ------------------------------------------------------------------------
 void loop() {
   if(twistCb_flag) {
-    motor_write(linear_vel, angular_vel); //if twistCb run, change motor speed
-    twistCb_flag = false;
+    //get motor speed
+    Kinematics::output motor_rpm;
+    motor_rpm = motors.getRPM(linear_vel, 0, angular_vel);  //robot cannot move in the y direction.
+    int left_pwm = map(motor_rpm.motor1, -MAX_MOTOR_RPM, MAX_MOTOR_RPM, -MAX_PWM_WRITE, MAX_PWM_WRITE);
+    int right_pwm = map(motor_rpm.motor2, -MAX_MOTOR_RPM, MAX_MOTOR_RPM, -MAX_PWM_WRITE, MAX_PWM_WRITE);
+    motor_write(left_pwm, right_pwm);
+  
+    twistCb_flag = false; //reset flag
   }
-
+  
   //run each loop
   nh.spinOnce();
   delay(50);
